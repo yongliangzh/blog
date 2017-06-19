4a052a27f06ef4ef8e0650a1284dda1fcd47ac58
和光同尘 <yongliang_zh@163.com>
2017-06-19 16:10:25 +0800

    add blog-templet

---
+++ js/blog-templet.js
@@ -0,0 +1,13 @@
+﻿var nowposition = 0;
+$(window).scroll(function () {
+    if ($(window).scrollTop() > nowposition) {
+        $(".public-header").animate({
+            height: 'hide',
+        });
+    } else {
+        $(".public-header").animate({
+            height: 'show',
+        });
+    }
+    nowposition = $(window).scrollTop();
+});
