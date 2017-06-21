2db557451a726eb87c193896b026c261e18416af
和光同尘 <yongliang_zh@163.com>
2017-06-21 18:15:00 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
-﻿var BlogIndex = 0;
-var BlogsNum = 0;
+﻿var BlogIndex ;
+var BlogsNum ;
 $(document).ready(function () {
-
+    BlogIndex = 0;
+    BlogsNum = 0;
     $(".container-r").find(".panel").each(function () {
         if (BlogIndex < 5) {
             $(this).show();
             BlogIndex++;
-            
         } else {
             $(this).hide();
         }
+        $(this).attr("isshow", "1");
         BlogsNum++;
     })
 });
@@ -20,13 +21,17 @@ function prevlist() {
     }
     var BlogIndexTemp = 0;
     $(".container-r").find(".panel").each(function () {
-        if (BlogIndexTemp >= BlogIndex - 10 && BlogIndexTemp < BlogIndex-5) {
-            $(this).show();
-
+        if ($(this).attr("isshow") == "1") {
+            if (BlogIndexTemp < BlogIndex - 5 && BlogIndexTemp >= BlogIndex - 10) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+            BlogIndexTemp++;
         } else {
             $(this).hide();
         }
-        BlogIndexTemp++;
+        
     });
     BlogIndex -= 5;
 }
@@ -36,16 +41,87 @@ function nextlist() {
         swal("没有更多可以加载了", "╮（╯＿╰）╭");
         return;
     }
-  
     var BlogIndexTemp = 0;
     $(".container-r").find(".panel").each(function () {
-        if (BlogIndexTemp < BlogIndex + 5 && BlogIndexTemp >= BlogIndex) {
-            $(this).show();
-
+        if ($(this).attr("isshow") == "1") {
+            if (BlogIndexTemp < BlogIndex + 5 && BlogIndexTemp >= BlogIndex) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+            BlogIndexTemp++;
         } else {
             $(this).hide();
         }
-        BlogIndexTemp++;
+
+        
     });
     BlogIndex+=5;
+}
+
+$('#SearchText').bind('input propertychange', function () {
+    BlogIndex = 0;
+    BlogsNum = 0;
+    $(".container-r").find(".panel").each(function () {
+        if ($(this).html().replace(/<.*?>/ig, "").indexOf($('#SearchText').val()) != -1) {
+            $(this).attr("isshow", "1");
+            if (BlogIndex < 5) {
+                $(this).show();
+
+                BlogIndex++;
+            } else {
+                $(this).hide();
+            }
+            BlogsNum++;
+        } else {
+            $(this).attr("isshow", "0");
+            $(this).hide();
+        }
+        
+    })
+   
+});
+
+function findupdate(time) {
+    BlogIndex = 0;
+    BlogsNum = 0;
+    $(".container-r").find(".panel").each(function () {
+        if (time == $(this).attr("updatetime")) {
+            $(this).attr("isshow", "1");
+            if (BlogIndex < 5) {
+                $(this).show();
+                BlogIndex++;
+            } else {
+                $(this).hide();
+            }
+            BlogsNum++;
+        } else {
+            $(this).attr("isshow", "0");
+            $(this).hide();
+        }
+
+    });
+}
+
+function selecttype(type) {
+    BlogIndex = 0;
+    BlogsNum = 0;
+    $(".container-r").find(".panel").each(function () {
+
+        if (type == $(this).attr("blogtype")) {
+            
+            $(this).attr("isshow", "1");
+            if (BlogIndex < 5) {
+                $(this).show();
+                BlogIndex++;
+            } else {
+                $(this).hide();
+            }
+            BlogsNum++;
+        } else {
+            $(this).attr("isshow", "0");
+            $(this).hide();
+        }
+
+    });
 }
