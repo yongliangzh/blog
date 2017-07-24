17df9f2311cf9c62dbdbe115695eb6a69008f7a9
和光同尘 <yongliang_zh@163.com>
2017-07-24 14:53:14 +0800

    update

---
+++ blogs/201707/mooc/js/index.js
@@ -0,0 +1,128 @@
+$(".search-input").focus(function(){
+  $("#prompt").css("display","none");
+});
+
+$(".search-input").blur(function(){
+    if($(this).val()==""){
+      $("#prompt").css("display","inline-block");
+  }
+});
+
+function showcart(){
+    $(".shop-panel").show();
+}
+
+function hidecart(){
+    $(".shop-panel").hide();
+}
+
+function changebgd(thislabel){
+    $(thislabel).css("background-color","#c9cdd1");
+}
+
+function changebgl(thislabel){
+    $(thislabel).css("background-color","#d9dde1");
+}
+function showmypanel(){
+    $(".my-panel").show();
+
+}
+function hidemypanel(){
+    $('.my-panel').hide();
+}
+
+function search(){
+    window.open("http://www.imooc.com/search/?words="+$("#search-text").val()); 
+}
+
+        $(function () {
+            var container = $('#container');
+            var list = $('#list');
+            var buttons = $('#buttons span');
+            var prev = $('#prev');
+            var next = $('#next');
+            var index = 1;
+            var len = 6;
+            var interval = 5000;
+            var timer1;
+
+            function animate (offset) {
+                var left = parseInt(list.css('left')) + offset;//获得最后的left
+                //偏移量大于零向左，小于零向右
+                if (offset>0) {
+                    offset = '+=' + offset;
+                }
+                else {
+                    offset = '-=' + Math.abs(offset);
+                }
+                list.animate({'left': offset}, 500, function () {
+                    if(left > -200){
+                        list.css('left', -1200 * len);
+                    }//到最后一张重置样式
+                    if(left < (-1200 * len)) {
+                        list.css('left', -1200);
+                    }//到第一张重置样式
+                });
+            }
+
+            function showButton() {
+                buttons.eq(index-1).addClass('on').siblings().removeClass('on');
+            }
+
+            function play() {
+                timer1 = setTimeout(function () {
+                    next.trigger('click');
+                    play();
+                }, interval);//设置一个定时器，模仿向右的点击事件，3秒一下
+            }
+            function stop() {
+                clearTimeout(timer1);//清除定时器
+            }
+
+            next.bind('click', function () {
+                if (list.is(':animated')) {
+                    return;
+                }//如果轮播图在动画中就不动
+                if (index == 6) {
+                    index = 1;
+                }//到了第5张轮播图重置为第3张
+                else {
+                    index += 1;
+                }  //当前轮播图加1
+                animate(-1200);//轮播图的left-1200
+                showButton();
+            });
+
+            prev.bind('click', function () {
+                if (list.is(':animated')) {
+                    return;
+                }
+                if (index == 1) {
+                    index = 6;
+                }
+                else {
+                    index -= 1;
+                }
+                animate(1200);
+                showButton();
+            });
+
+            buttons.each(function () {
+                 $(this).bind('click', function () {
+                     if (list.is(':animated') || $(this).attr('class')=='on') {//如果在动画中 或者是当前选中的就不动
+                         return;
+                     }
+                     var myIndex = parseInt($(this).attr('index'));
+                     var offset = -1200 * (myIndex - index);
+
+                     animate(offset);
+                     index = myIndex;//获得选中的按钮的index赋给当前
+                     showButton();
+                 })
+            });
+
+            container.hover(stop, play);//如果鼠标在轮播图上就停止，不在就显示
+
+            play();//自动播放轮播图
+
+        });
---
+++ blogs/201707/mooc/js/megadropdown.js
@@ -0,0 +1,123 @@
+﻿$(document).ready(function () {
+    var sub = $('#sub');
+    var activeRow;
+    var activeMenu;
+    var timer;
+    var mouseInSub = false;
+    sub.on('mouseenter', function (e) {
+        mouseInSub = true;
+    }).on('mouseleave', function (e) {
+        mouseInSub = false;
+    })
+    var mouseTrack = [];
+    var moveHandler = function (e) {
+        mouseTrack.push({
+            x: e.pageX,
+            y: e.pageY
+        })
+        if (mouseTrack.length > 3) {
+            mouseTrack.shift();
+        }
+    }
+    $('#list-menu').on('mouseenter', function (e) {
+        sub.removeClass('none');
+
+        $(document).bind('mousemove', moveHandler);
+
+    }).on('mouseleave', function (e) {
+        sub.addClass('none');
+
+
+        if (activeRow) {
+            activeRow.removeClass('active');
+            activeRow = null;
+        }
+        if (activeMenu) {
+            activeMenu.addClass('none');
+            activeMenu = null;
+        }
+        $(document).unbind('mousemove', moveHandler)
+    }).on('mouseenter', '.item-out', function (e) {
+        if (!activeRow) {
+            activeRow = $(e.target).addClass('active');
+            activeMenu = $('#' + activeRow.data('id'));
+            activeMenu.removeClass('none');
+            return;
+        }
+        if (timer) {
+            clearTimeout(timer);
+        }
+        var currMousePos = mouseTrack[mouseTrack.length - 1];;
+        var leftCorner = mouseTrack[mouseTrack.length - 2];
+
+        var delay = needDelay(sub, leftCorner, currMousePos);
+
+        if (delay) {
+            timer = setTimeout(function () {
+                if (mouseInSub) {
+                    return;
+                }
+                activeRow.removeClass('active');
+                activeMenu.addClass('none');
+
+                activeRow = $(e.target);
+                activeRow.addClass('active');
+                activeMenu = $('#' + activeRow.data('id'));
+                activeMenu.removeClass('none');
+            }, 300)
+        } else {
+            var prevActiveRow = activeRow;
+            var prevActiveMenu = activeMenu;
+
+            activeRow = $(e.target);
+            activeMenu = $('#' + activeRow.data('id'));
+
+            prevActiveRow.removeClass('active');
+            prevActiveMenu.addClass('none');
+
+            activeRow.addClass('active');
+            activeMenu.removeClass('none');
+        }
+    })
+})
+
+function sameSign(a, b) {
+    return (a ^ b) >= 0
+}
+
+function vector(a, b) {
+    return {
+        x: b.x - a.x,
+        y: b.y - a.y
+
+    }
+}
+
+
+function vectorProduct(v1, v2) {
+    return v1.x * v2.y - v2.x * v1.y;
+}
+
+function isPointInTrangle(p, a, b, c) {
+    var pa = vector(p, a);
+    var pb = vector(p, b);
+    var pc = vector(p, c);
+    var t1 = vectorProduct(pa, pb);
+    var t2 = vectorProduct(pb, pc);
+    var t3 = vectorProduct(pc, pa);
+    var a = sameSign(t1, t2) && sameSign(t2, t3)
+    return sameSign(t1, t2) && sameSign(t2, t3);
+}
+
+function needDelay(elem, leftCorner, currMousePos) {
+    var offset = elem.offset()
+    var topLeft = {
+        x: offset.left,
+        y: offset.top
+    }
+    var bottomLeft = {
+        x: offset.left,
+        y: offset.top + elem.height(),
+    }
+    return isPointInTrangle(currMousePos, leftCorner, topLeft, bottomLeft)
+}
